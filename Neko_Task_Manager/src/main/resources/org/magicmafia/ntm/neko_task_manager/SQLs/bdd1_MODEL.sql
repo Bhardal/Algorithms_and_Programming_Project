2d6838978d04b6a6e487d6cb6c176b7200e326f6
Rémy Maillard <remy85603@gmail.com>
2025-01-15 19:51:42 +0100

    MAJOR: ci: Added the basis of the sql database

---
+++ Neko_Task_Manager/src/main/resources/org/magicmafia/ntm/neko_task_manager/SQLs/bdd1_MODEL.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Employees (
+Name VARCHAR(50),
+EmployeeID INT PRIMARY KEY,
+Projects table
+);
+
+CREATE TABLE Tasks (
+Name VARCHAR(50),
+Priority INT,
+Status VARCHAR(50),
+DueDate datetime,
+Comment VARCHAR(500),
+Description VARCHAR(500)
+)
