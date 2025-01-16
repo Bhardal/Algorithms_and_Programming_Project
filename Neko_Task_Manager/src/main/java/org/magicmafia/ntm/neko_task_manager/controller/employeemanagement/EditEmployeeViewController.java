b48823f2258202e22f2e029ea65924878498c97a
KoRIOz675 <thomas@mafille.net>
2025-01-16 17:03:15 +0100

    MAJOR: revert: Reverted the reverse

--- Neko_Task_Manager/src/main/java/org/magicmafia/ntm/neko_task_manager/controller/employeemanagement/EditEmployeeViewController.java
+++ Neko_Task_Manager/src/main/java/org/magicmafia/ntm/neko_task_manager/controller/employeemanagement/EditEmployeeViewController.java
@@ -55,6 +55,7 @@ public class EditEmployeeViewController {
             Stage stage = (Stage) OKButton.getScene().getWindow();
             stage.close();
         }
+        
     }
 
     public void setEmployeeManagementViewController(EmployeeManagementViewController employeeManagementViewController) {
