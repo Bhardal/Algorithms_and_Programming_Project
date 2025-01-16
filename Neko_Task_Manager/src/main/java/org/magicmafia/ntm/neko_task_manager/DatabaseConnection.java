f87ab096c713b0ec27495f9d70ea67fb1d42e423
Rémy Maillard <remy85603@gmail.com>
2025-01-16 15:56:21 +0100

    MEDIUM: ci: Added a way to create a table

--- Neko_Task_Manager/src/main/java/org/magicmafia/ntm/neko_task_manager/DatabaseConnection.java
+++ Neko_Task_Manager/src/main/java/org/magicmafia/ntm/neko_task_manager/DatabaseConnection.java
@@ -1,8 +1,6 @@
 package org.magicmafia.ntm.neko_task_manager;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseConnection {
     public static void main(String[] args) {
@@ -11,6 +9,18 @@ public class DatabaseConnection {
             if (conn != null) {
                 System.out.println("Connected to the database.");
             }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        String sql = "CREATE TABLE IF NOT EXISTS Employees ("
+                + " Name VARCHAR(50) NOT NULL,"
+                + " EmployeeID INT PRIMARY KEY,"
+                + " Projects MEDIUMTEXT);";
+
+        try (Connection conn = DriverManager.getConnection(url); Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println("Table created.");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
