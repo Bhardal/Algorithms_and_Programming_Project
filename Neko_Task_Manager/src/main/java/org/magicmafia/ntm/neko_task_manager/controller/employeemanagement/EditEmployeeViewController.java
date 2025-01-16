775b3be67528d6df8210d87137630e66192a1b3d
Rémy Maillard <remy85603@gmail.com>
2025-01-16 16:30:40 +0100

    MAJOR: ci: Added a way to update employee information from the database

--- Neko_Task_Manager/src/main/java/org/magicmafia/ntm/neko_task_manager/controller/employeemanagement/EditEmployeeViewController.java
+++ Neko_Task_Manager/src/main/java/org/magicmafia/ntm/neko_task_manager/controller/employeemanagement/EditEmployeeViewController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import java.sql.*;
 
 public class EditEmployeeViewController {
     @FXML
@@ -41,6 +42,21 @@ public class EditEmployeeViewController {
             a.show();
         }else {
             Stage stage = (Stage) closeButton.getScene().getWindow();
+
+            String url = "jdbc:sqlite:mydatabase.db";
+            String sql = "DELETE FROM employees WHERE employee_id = (?); INSERT INTO Employees(name, EmployeeID, Projects) VALUES(?, ?, ?)";
+            try (Connection conn = DriverManager.getConnection(url);
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.setInt(1, OldEmployeeIDInt);
+                pstmt.setString(2, newEmployeeNameText);
+                pstmt.setInt(3, OldEmployeeIDInt);
+                pstmt.setString(4, ProjectList);
+                pstmt.executeUpdate();
+                System.out.println("Data updated.");
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+
             stage.close();
         }
     }
